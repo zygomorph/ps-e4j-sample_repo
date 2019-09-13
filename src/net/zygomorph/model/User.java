991e0770cc8fc1016c4e67bf38ce4edd68230510
David Richardson <djrichardson@leggmason.com>
2019-09-13 16:08:18 +0100

    updated the toString() method

--- src/net/zygomorph/model/User.java
+++ src/net/zygomorph/model/User.java
@@ -23,7 +23,7 @@ public class User {
 
     @Override
     public String toString() {
-        return "User [name=" + name + "]";
+        return "User [name=" + name + ", age=" + age + "]";
     }
         
 }
