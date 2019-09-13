32ff2e2664a0594f997fd7851835d13fa0dec857
David Richardson <djrichardson@leggmason.com>
2019-09-13 15:56:46 +0100

    added a toString() method

--- src/net/zygomorph/model/User.java
+++ src/net/zygomorph/model/User.java
@@ -11,5 +11,10 @@ public class User {
     public void setName(String name) {
         this.name = name;
     }
-    
+
+    @Override
+    public String toString() {
+        return "User [name=" + name + "]";
+    }
+        
 }
