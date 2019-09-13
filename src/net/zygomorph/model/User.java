5bb19a4133e57b0f141e63128be08c3a3070c9f8
David Richardson <djrichardson@leggmason.com>
2019-09-13 15:35:57 +0100

    Added getter and setter for the name field

--- src/net/zygomorph/model/User.java
+++ src/net/zygomorph/model/User.java
@@ -3,5 +3,13 @@ package net.zygomorph.model;
 public class User {
 
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
     
 }
